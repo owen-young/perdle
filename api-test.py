51fa0d335df166775587c55fcefe10644d055ec3
owen.young0@protonmail.com <owen.young8699@gmail.com>
2022-09-02 00:23:09 -0500

    Updated some test scripts.

--- api-test.py
+++ api-test.py
@@ -10,9 +10,11 @@ site = wikipedia('en')
 
 def test():
     # Search for all backlinks to Tom Brady
+    count = 0
     for p in site.query(bltitle='Tom_Brady', list=['backlinks'], blnamespace=0):
-         print(p)
+         count += 1
 
+    print("wikipedia API count:", count)
 
     # use another API to get the number of backlinks
     response = requests.get("http://linkcount.toolforge.org/api/?page=Tom_Brady&project=en.wikipedia.org")
@@ -26,7 +28,7 @@ def test():
         'User-Agent': 'Perdle / owen.young0@protonmail.com'
     }
     response = requests.get("https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/user/Tom_Brady/monthly/20220301/" + current_date, headers=req_headers)
-    print(response.json())
+    # print(response.json())
 
 # Loop through all pages with Infobox and see if they're people using Dbpedia.
 def name_loop():
@@ -56,6 +58,7 @@ def name_loop():
                     except Exception as e:
                         print(e)
 
+test()
 
 
 
--- asyncio_test.py
+++ asyncio_test.py
@@ -1,5 +1,6 @@
 import aiohttp
 import asyncio
+import re
 
 async def main():
 	query = '''
@@ -7,7 +8,7 @@ async def main():
 	WHERE {
 	  ?person wdt:P31 wd:Q5;
 		  wdt:P569 ?birth;
-	  FILTER (?birth >= "1970-01-01"^^xsd:dateTime && ?birth < "1980-01-01"^^xsd:dateTime) .
+	  FILTER (?birth >= "1995-01-01"^^xsd:dateTime && ?birth < "2000-01-01"^^xsd:dateTime) .
 	  ?person wikibase:sitelinks ?sitelinks .
 	  ?article schema:about ?person .
 	  ?article schema:isPartOf <https://en.wikipedia.org/>.
@@ -16,7 +17,7 @@ async def main():
 	  }
 	}
 	ORDER BY DESC(?sitelinks)
-	LIMIT 400
+	LIMIT 100
 	'''
 	async with aiohttp.ClientSession() as session:
 		tasks = []
@@ -25,14 +26,15 @@ async def main():
 		query_result = await asyncio.gather(*tasks)
 
 
+
 async def wd_sparql_query(session, query):
 	url = 'https://query.wikidata.org/sparql'
 	wiki_params = {'format': 'json', 'query': query}
-	
+
 	async with session.get(url, params=wiki_params) as resp:
 		query_results = await resp.read()
 		print(query_results)
-	
+
 	return
 
 asyncio.run(main())
